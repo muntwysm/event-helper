74b5f061016f0350d64451b8efb6257b0accc67b
muntwysm <smuntwyler@zis.ch>
2011-02-19 17:45:31 +0100

    added minQty to event edit

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,9 +1,12 @@
 module EventsHelper
 
-def add_requirement_link(name)
-	link_to_function name do |page|
-		page.insert_html :bottom, :requirements, :partial => 'requirement' , :object => Requirement.new
+	def add_requirement_link(name)
+		link_to_function name do |page|
+			page.insert_html :bottom, :requirements, :partial => 'requirement' , :object => Requirement.new
+		end
 	end
-end
 
+	def count_contributions_for(event_id,item_id)
+		return Contribution.sum("qty", :conditions => ["event_id = ? AND item_id = ?", event_id, item_id])
+	end
 end
