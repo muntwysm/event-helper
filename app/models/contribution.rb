e3caa2e1fa2c292dfb959ba62df18639cee8a642
muntwysm <smuntwyler@zis.ch>
2011-02-20 12:01:12 +0100

    search fixes

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -13,8 +13,7 @@ class Contribution < ActiveRecord::Base
 
 	def self.search(search)
 		if search
-		  #find(:all, :conditions => ['LOWER(email)
-						 LIKE ?', "%#{search.downcase}%"])
+		  #find(:all, :conditions => ['LOWER(email) LIKE ?', "%#{search.downcase}%"])
 		  find(:all, :conditions => ['email LIKE ?', "%#{search}%"])
 		else
 			find(:all)
